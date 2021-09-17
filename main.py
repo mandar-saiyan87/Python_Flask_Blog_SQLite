745033acc649cb0d7155a87d24c7b48cb3123e59
mandar-saiyan87 <mdman2257@gmail.com>
2021-09-17 10:33:54 +0530

    URI updated2

--- main.py
+++ main.py
@@ -32,7 +32,8 @@ gravatar = Gravatar(app,
 uri = os.environ.get('DATABASE_URL')
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
-app.config['SQLALCHEMY_DATABASE_URI'] = uri, 'sqlite:///blog1.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
+# 'sqlite:///blog1.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
